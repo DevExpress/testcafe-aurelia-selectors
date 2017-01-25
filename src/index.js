68c66dfcbd9e9b9fc05412964cd28fb81c1f44cd
miherlosev@mail.ru <Ein7Onai>
2017-01-25 11:26:50 +0300

    mark internal methods as '_xxx'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { Selector } from 'testcafe';
 
 class AureliaSelector {
-    createSelector (bindingDescriptor, selectorInit) {
+    _createSelector (bindingDescriptor, selectorInit) {
         const elementSelector = `*[${bindingDescriptor.type}\\.${bindingDescriptor.command}="${bindingDescriptor.expression}"]`;
 
         return selectorInit ? Selector(selectorInit).find(elementSelector)
@@ -10,8 +10,8 @@ class AureliaSelector {
 
     /*valueBind*/
 
-    createBindSelector (type, expression, selectorInit) {
-        return this.createSelector({
+    _createBindSelector (type, expression, selectorInit) {
+        return this._createSelector({
             type:       type,
             command:    'bind',
             expression: expression
@@ -19,25 +19,25 @@ class AureliaSelector {
     }
 
     byValueBind (expression, parentSelector) {
-        return this.createBindSelector('value', expression, parentSelector);
+        return this._createBindSelector('value', expression, parentSelector);
     }
 
     byShowBind (expression, parentSelector) {
-        return this.createBindSelector('show', expression, parentSelector);
+        return this._createBindSelector('show', expression, parentSelector);
     }
 
     byCheckedBind (expression, parentSelector) {
-        return this.createBindSelector('checked', expression, parentSelector);
+        return this._createBindSelector('checked', expression, parentSelector);
     }
 
     byFocusBind (expression, parentSelector) {
-        return this.createBindSelector('focus', expression, parentSelector);
+        return this._createBindSelector('focus', expression, parentSelector);
     }
 
     /*eventDelegate*/
 
-    createDelegateSelector (event, expression, selectorInit) {
-        return this.createSelector({
+    _createDelegateSelector (event, expression, selectorInit) {
+        return this._createSelector({
             type:       event,
             command:    'delegate',
             expression: expression
@@ -45,11 +45,11 @@ class AureliaSelector {
     }
 
     byDbClickDelegate (expression, parentSelector) {
-        return this.createDelegateSelector('dblclick', expression, parentSelector);
+        return this._createDelegateSelector('dblclick', expression, parentSelector);
     }
 
     byClickDelegate (expression, parentSelector) {
-        return this.createDelegateSelector('click', expression, parentSelector);
+        return this._createDelegateSelector('click', expression, parentSelector);
     }
 }
 
